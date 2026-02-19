68a3e20487663287b1b1fef68a842e7ac35bea62
navneetj900-hub <navneetj900@gmail.com>
2026-02-19 18:25:52 +0530

    Refactor server.js to use app and config imports

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,6 @@
-console.log("Backend started");
+import app from "./app.js";
+import { config } from "./config.js";
+
+app.listen(config.PORT, () =>
+  console.log("Server running on port", config.PORT)
+);
